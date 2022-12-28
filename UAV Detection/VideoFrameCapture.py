a8b22ad52413b16e0b5d5c9d2d4d3db89c3aaefb
egoistpizza <yusufozcetin@protonmail.com>
2022-12-29 02:31:08 +0300

    Merge branch 'main' of https://github.com/egoistpizza/KayraUAV

--- UAV Detection/VideoFrameCapture.py
+++ UAV Detection/VideoFrameCapture.py
@@ -1,73 +1,9 @@
-<<<<<<< HEAD
 """
-* Bu kod OpenCV modülünü kullanarak videolardan düzenli aralıklarla ekran görüntüsü alınmasını ve kaydedilmesini sağlamaktadır.
-? Sorularınızı discord üzerinden iletebilirsiniz
-TODO: Kod denenerek dataset oluşturmaya başlanacak.
-TODO: Videoları otomatik olarak klasörden bulmalı -KAPANDI-
-"""
-
-import cv2 as cv
-import os
-import time
-from math import floor,ceil
-
-
-#*---Önemli Değişkenler---
-video_dir = "/Videos" # Videoya ait dizini seçiyoruz. Şu an da "%current_working_directory%/Videos" dizini seçili
-save_dir = "data" #Ekran görüntülerinin kayıt edileceği dizini seçiyoruz
-interval = 2 # Interval in seconds
-img_extension = ".png" # Target image type
-
-
-videos = [] # Verilen klasörde otomatik olarak bulunan videolar burada depolanacaktır.
-video_exts = [".mp4",".mov",".webm",".wmv",".avi","mkv"] # Sık kullanılan video uzantıları
-
-for item in os.listdir(video_dir):
-    if os.path.isfile(os.path.join(video_dir,item)):
-        if os.path.splitext(item)[1].lower() in video_exts:
-            videos.append(os.path.join(video_dir,item))
-
-
-try:
-    if not os.path.exists(save_dir):   # Ekran görüntülerini depolamak için yeni klasör açıyoruz.
-        os.makedirs(save_dir)
-except OSError:
-    print('Error: Creating directory of data')
-
-def getFramesFromVideo(video_file):
-    cap = cv.VideoCapture(video_file) #The capture object
-        
-    fps = cap.get(cv.CAP_PROP_FPS)                  #FPS of the video
-    total_frames = cap.get(cv.CAP_PROP_FRAME_COUNT) #total frame count of the video
-    duration = total_frames/fps                     #duration of the video
-    max_iter = floor(duration/interval)
-
-    for i in range(max_iter):
-        cap.set(cv.CAP_PROP_POS_FRAMES, i*interval*fps-1)
-        ret, frame = cap.read()
-        if ret:
-            cv.imwrite(os.path.join(save_dir,"SS_{2}{0}{1}".format(str(i),img_extension,floor(time.time_ns()/1000000))), frame)
-        else:
-            print("An error occured while reading from the capture.")
-            break
-    cap.release()
-
-    print("Total of {} images saved.".format(str(max_iter)))
-    print("Save directory: {}".format(save_dir))
-
-
-for video in videos:
-    getFramesFromVideo(video)
-
-
-print("Exiting in 10 seconds")
-time.sleep(10) #Giving time for the console to see the error
-=======
-"""
-* Bu kod OpenCV modülünü kullanarak videolardan düzenli aralıklarla ekran görüntüsü alınmasını ve kaydedilmesini sağlamaktadır.
-? Sorularınızı discord üzerinden iletebilirsiniz
-TODO: Kod denenerek dataset oluşturmaya başlanacak.
-TODO: Videoları otomatik olarak klasörden bulmalı -KAPANDI-
+* Bu kod OpenCV modülü kullanılarak bir klasördeki videolardan periyodik olarak frame yakalanmasını ve kaydedilmesini sağlamaktadır.
+* Kullanım amacı, dataset kaynağı olan videolardan frame elde edilerek labeling işlemine tabi tutulmasıdır.
+? Sorularınız için: kayraproject@gmail.com
+! OS modülünün dosya işlemlerini gerçekleştirebilmesi için yönetici olarak çalıştırınız.
+TODO: Kod dataset oluşturmak için hazır, tüm testler başarıyla tamamlandı, proje kapsamında kullanılabilir.
 """
 
 import cv2 as cv
@@ -75,55 +11,54 @@ import os
 import time
 from math import floor,ceil
 
+# * ---Önemli Değişkenler---
 
-#*---Önemli Değişkenler---
-video_dir = "/Videos" # Videoya ait dizini seçiyoruz. Şu an da "%current_working_directory%/Videos" dizini seçili
-save_dir = "data" #Ekran görüntülerinin kayıt edileceği dizini seçiyoruz
-interval = 2 # Interval in seconds
-img_extension = ".png" # Target image type
+video_dir = "/home/egoist/Videolar/dataset-test/Videos"   # Videoya ait dizini seçiyoruz. Varsayılan olarak "%current_working_directory%/Videos" dizini seçili
+save_dir = os.path.join(video_dir,"frames")   # Framelerin kaydedileceği dizini seçiyoruz.
+interval = 2   # Saniye cinsinden periyot (bekleme süresi).
+img_extension = ".png"   # Framelerin kaydedileceği dosya uzantısı.
 
-
-videos = [] # Verilen klasörde otomatik olarak bulunan videolar burada depolanacaktır.
-video_exts = [".mp4",".mov",".webm",".wmv",".avi","mkv"] # Sık kullanılan video uzantıları
+videos = []   # Verilen klasördeki videolara ait uzantıların toplanacağı lise.
+video_exts = [".mp4",".mov",".webm",".wmv",".avi","mkv"]   # Desteklenen video uzantıları (genişletilebilir).
 
 for item in os.listdir(video_dir):
     if os.path.isfile(os.path.join(video_dir,item)):
         if os.path.splitext(item)[1].lower() in video_exts:
             videos.append(os.path.join(video_dir,item))
 
+print(f"{len(videos)} videos were found in the specified folder.")
 
 try:
-    if not os.path.exists(save_dir):   # Ekran görüntülerini depolamak için yeni klasör açıyoruz.
+    if not os.path.exists(save_dir):   # Eğer bulunmuyorsa, ekran görüntülerini depolamak için yeni bir klasör açıyoruz.
         os.makedirs(save_dir)
+        
 except OSError:
-    print('Error: Creating directory of data')
+    print('OS_Error: Creating directory of data.\nPlease run the code as administrator to complete the file operations.\nIf you continue to receive errors, you can create an issue or contact us. (Contact: kayraproject@gmail.com)')
 
 def getFramesFromVideo(video_file):
-    cap = cv.VideoCapture(video_file) #The capture object
-        
-    fps = cap.get(cv.CAP_PROP_FPS)                  #FPS of the video
-    total_frames = cap.get(cv.CAP_PROP_FRAME_COUNT) #total frame count of the video
-    duration = total_frames/fps                     #duration of the video
-    max_iter = floor(duration/interval)
+    cap = cv.VideoCapture(video_file)   # Video yakalama objesi.
+    
+    fps = cap.get(cv.CAP_PROP_FPS)   # Videoya ait FPS değeri.
+    total_frames = cap.get(cv.CAP_PROP_FRAME_COUNT)   # Videodaki toplam frame sayısı.
+    duration = total_frames/fps   # Saniye cinsinden video süresi.
+    max_iter = floor(duration/interval)   # Belirlenen periyotta çekilebilecek maksimum frame sayısı.
 
     for i in range(max_iter):
         cap.set(cv.CAP_PROP_POS_FRAMES, i*interval*fps-1)
         ret, frame = cap.read()
         if ret:
-            cv.imwrite(os.path.join(save_dir,"SS_{2}{0}{1}".format(str(i),img_extension,floor(time.time_ns()/1000000))), frame)
+            cv.imwrite(os.path.join(save_dir,f"frame_{floor(time.time_ns()/1000000)}_{str(i)}{img_extension}"), frame)
+            print(f"\nThe frame has been successfully saved to the directory: {save_dir}, {i}")
         else:
-            print("An error occured while reading from the capture.")
+            print("A problem occurred while capturing the frame.\nTry again by changing the interval value.\nIf it is repeated, it means there is a video reading error.\nYou can open an issue or contact us for help or bugfix. (Contact: kayraproject@gmail.com)")
             break
     cap.release()
 
-    print("Total of {} images saved.".format(str(max_iter)))
-    print("Save directory: {}".format(save_dir))
-
+    print(f"\n\n\nTotal of {str(max_iter)} images saved.")
+    print(f"Save directory: {save_dir}")
 
 for video in videos:
     getFramesFromVideo(video)
 
-
-print("Exiting in 10 seconds")
-time.sleep(10) #Giving time for the console to see the error
->>>>>>> 2b3831fe6531b7422d69f51f1c5ba0d9e49074bd
+print("\n\n\nAll videos in the destination folder have been processed successfully.")
+print("\nThere is nothing to do.")
